3d7570b16ed363c1cf91aafe03c1500b6c6cfdff
Saad Farhan <56558687+saadfarhan124@users.noreply.github.com>
2023-06-21 13:30:53 +0500

    added delete mobile api

--- services/customers/app/routes/customer_routes.py
+++ services/customers/app/routes/customer_routes.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from typing import List
 from app.schemas import CustomerCreate, Individual, IndividualList, IndividualUpdate, AddressUpdate, EmployerUpdate
 from app.schemas import BussinessCustomerCreate, Business, BussinessUpdate, BusinessList
-from app.schemas import EmailAddressDelete
+from app.schemas import EmailAddressDelete, MobileNumberDelete
 from app.database.database import get_db
 from app.utils import customer as customer_crud, email_address_crud, employer_crud, address_crud, mobile_number_crud, individual_crud, bussiness_crud
 from app.constants import BusinessType, get_business_type_string, OccupationStatus, get_occupation_status_string, MaritalStatus, get_marital_status_string, Gender, get_gender_string
@@ -66,16 +66,12 @@ def update_individual(individual: IndividualUpdate, db: Session = Depends(get_db
     return individual_crud.update(db, db_obj=individual_obj, update_schema=individual)
 
 # Update Address
-
-
 @router.put("/customers/individual/address")
 def update_customer_address(address: AddressUpdate, db: Session = Depends(get_db)):
     address_obj = address_crud.get(db, id=address.id)
     return address_crud.update(db, db_obj=address_obj, update_schema=address)
 
 # Update Employer
-
-
 @router.put("/customers/individual/employer")
 def update_customer_individual(employer: EmployerUpdate, db: Session = Depends(get_db)):
     employer_obj = employer_crud.get(db, id=employer.id)
@@ -87,10 +83,11 @@ def update_customer_individual(employer: EmployerUpdate, db: Session = Depends(g
 def delete_email_address(email_address: EmailAddressDelete, db: Session = Depends(get_db)):
     return email_address_crud.remove(db, id=email_address.id)
 
+@router.delete("/customers/mobile_number")
+def delete_mobile_address(mobile_number: MobileNumberDelete, db: Session = Depends(get_db)):
+    return mobile_number_crud.remove(db, id=mobile_number.id)
 
 # Create Customer Business
-
-
 @router.post("/customers/business", response_model=Business)
 def create_customer_business(customer: BussinessCustomerCreate, db: Session = Depends(get_db)):
     cust_obj = customer_crud.create(db, create_schema=customer)
@@ -105,15 +102,11 @@ def get_business_by_id(id: int, db: Session = Depends(get_db)):
     return bussiness_crud.get(db, id=id)
 
 # Get All Customers Business
-
-
 @router.get("/customers/business/{offset}/{limit}", response_model=BusinessList)
 def get_all_businesses(offset: int, limit: int, db: Session = Depends(get_db)):
     return BusinessList(customers=bussiness_crud.get_multi(db, offset=offset, limit=limit), count=bussiness_crud.get_count(db=db))
 
 # Update Business
-
-
 @router.put("/customers/business")
 def update_business(business: BussinessUpdate, db: Session = Depends(get_db)):
     business_obj = bussiness_crud.get(db, id=business.id)
